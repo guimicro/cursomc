fe59567ad55cd97c2cb82ffeb2cd20f2d53496fa
Guilherme Machado <guimicro@hotmail.com>
2020-01-07 14:37:31 -0300

    Criando repository e service para categoria

--- src/main/java/com/guilherme/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/guilherme/cursomc/resources/CategoriaResource.java
@@ -3,27 +3,39 @@ package com.guilherme.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guilherme.cursomc.domain.Categoria;
+import com.guilherme.cursomc.services.CategoriaService;
+
+import javassist.tools.rmi.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<Categoria>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+//		
+//		Categoria cat1 = new Categoria(1, "Informática");
+//		Categoria cat2 = new Categoria(2, "Escritório");
+//		
+//		List<Categoria> lista = new ArrayList<Categoria>();
+//		lista.add(cat1);
+//		lista.add(cat2);
+//		
+//		return lista;
 	}
 
 }
---
+++ src/main/java/com/guilherme/cursomc/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.guilherme.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.cursomc.domain.Categoria;
+import com.guilherme.cursomc.repositories.CategoriaRepository;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) throws ObjectNotFoundException {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+}
